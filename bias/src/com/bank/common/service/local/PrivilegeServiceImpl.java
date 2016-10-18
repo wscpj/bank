6c2306c846f7bd893752041468c083cf3bcd1fd2
gaoyakunsan <zzugyk@163.com>
2016-10-18 23:37:34 +0800

    权限管理

--- bias/src/com/bank/common/service/local/PrivilegeServiceImpl.java
+++ bias/src/com/bank/common/service/local/PrivilegeServiceImpl.java
@@ -1,4 +1,5 @@
 package com.bank.common.service.local;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,11 +17,12 @@ import com.bank.common.model.Privilege;
 import com.bank.common.model.Role;
 import com.bank.common.service.PrivilegeService;
 
-public class PrivilegeServiceImpl extends BaseService implements PrivilegeService {
+public class PrivilegeServiceImpl extends BaseService implements
+        PrivilegeService {
 
-	private static final long serialVersionUID = -319207069173729558L;
-	
-	private PrivilegeDao privilegeDao;
+    private static final long serialVersionUID = -319207069173729558L;
+
+    private PrivilegeDao privilegeDao;
 
     public void setPrivilegeDao(PrivilegeDao privilegeDao) {
         this.privilegeDao = privilegeDao;
@@ -31,7 +33,8 @@ public class PrivilegeServiceImpl extends BaseService implements PrivilegeServic
         List<PrivilegeDTO> privilegeDTOs = privilegeDao.findRolePrivileges();
         Map<String, List<Privilege>> rolePrivileges = new HashMap<String, List<Privilege>>();
         for (PrivilegeDTO privilegeDTO : privilegeDTOs) {
-            Privilege privilege = (Privilege) copyObject(privilegeDTO, Privilege.class);
+            Privilege privilege = (Privilege) copyObject(privilegeDTO,
+                    Privilege.class);
             privilege.setCreatedTime(privilegeDTO.getCreatedTime());
             if (rolePrivileges.get(privilegeDTO.getRoleCode()) == null) {
                 List<Privilege> privileges = new ArrayList<Privilege>();
@@ -41,29 +44,49 @@ public class PrivilegeServiceImpl extends BaseService implements PrivilegeServic
         }
         return rolePrivileges;
     }
-    
+
     @Override
-    public Integer findPrivilegeCount(Map<String, Object> map){
-    	return privilegeDao.findPrivilegeCount(map);
+    public Integer findPrivilegeCount(Map<String, Object> map) {
+        return privilegeDao.findPrivilegeCount(map);
     }
+
     @SuppressWarnings("unchecked")
-	@Override
-    public List<Privilege> findAllPrivilege(Map<String, Object> map){
-    	PaginationDTO<Role> paginationDTO = (PaginationDTO<Role>) AppContext.getContext().getObject(
-                AppConstants.PAGINATION_DTO);
+    @Override
+    public List<Privilege> findAllPrivilege(Map<String, Object> map) {
+        PaginationDTO<Role> paginationDTO = (PaginationDTO<Role>) AppContext
+                .getContext().getObject(AppConstants.PAGINATION_DTO);
         if (paginationDTO != null) {
-        	map.putAll(paginationDTO.getParameterMap());  
+            map.putAll(paginationDTO.getParameterMap());
             Integer count = findPrivilegeCount(map);
             paginationDTO.setTotalRowCount(count);
         }
-    	return privilegeDao.findAllPrivilege(map);
+        return privilegeDao.findAllPrivilege(map);
     }
+
     @Override
-    public Boolean addPrivilege(Privilege privilege){
-    	 SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-         String nowDate = sf.format(new Date());
-         privilege.setCreatedTime(nowDate);
-         privilege.setUpdatedTime(nowDate);
-    	return privilegeDao.add(privilege);
+    public Boolean addPrivilege(Privilege privilege) {
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String nowDate = sf.format(new Date());
+        privilege.setCreatedTime(nowDate);
+        privilege.setUpdatedTime(nowDate);
+        return privilegeDao.add(privilege);
+    }
+
+    @Override
+    public Privilege selectPrivilegeById(Integer id) {
+        return privilegeDao.getById(id);
+    }
+
+    @Override
+    public Boolean updatePrivilege(Privilege privilege) {
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String nowDate = sf.format(new Date());
+        privilege.setUpdatedTime(nowDate);
+        return privilegeDao.update(privilege);
+    }
+
+    @Override
+    public void deletePrivilegeByIds(List<Integer> listId) {
+        privilegeDao.deletePrivilegeByIds(listId);
     }
 }
